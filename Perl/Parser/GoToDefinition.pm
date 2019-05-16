7272c6fbfeb6839a362075a0115dfaf5a28364d4
Marc Reisner <reisner.marc@gmail.com>
2019-05-15 21:00:37 -0500

    Refactor to allow for different types of tokens

--- Perl/Parser/GoToDefinition.pm
+++ Perl/Parser/GoToDefinition.pm
@@ -1,6 +1,7 @@
 package Perl::Parser::GoToDefinition;
 
 use File::Spec;
+use Perl::Critic::Utils;
 use PPI;
 use PPI::Find;
 use Scalar::Util qw(blessed);
@@ -21,7 +22,8 @@ sub go_to_definition {
 
     # LSP defines position as 0-indexed, PPI defines it 1-indexed
     $line++; $column++;
-    my $match = find_symbol_at_location($document, $line, $column);
+    my @matches = find_elements_at_location($document, $line, $column);
+    my $match = find_symbol_at_location(@matches);
     return undef unless $match;
 
     my $definition = find_lexical_variable_declaration($document, $match);
@@ -30,7 +32,7 @@ sub go_to_definition {
     return ($definition->line_number - 1, $definition->visual_column_number - 1);
 }
 
-sub find_symbol_at_location {
+sub find_elements_at_location {
     my ($document, $line, $column) = @_;
 
     my $find = PPI::Find->new(sub {
@@ -38,24 +40,50 @@ sub find_symbol_at_location {
 
         $element->line_number == $line &&
         $element->visual_column_number <= $column &&
-        $element->visual_column_number + length($element->content) >= $column &&
-        ($element->isa('PPI::Token::Symbol') || $element->isa('PPI::Token::Cast'));
+        $element->visual_column_number + length($element->content) >= $column;
     });
 
     return unless $find->start($document);
-    my $match = $find->match; # let's just use the first match
-    $find->finish;
 
-    return undef unless $match;
-
-    # find the thing we're casting, if this is a cast
-    if ($match->isa('PPI::Token::Cast')) {
-        while ($match = $match->next_sibling) {
-            last if $match->isa('PPI::Token::Symbol');
-        }
+    my @matches;
+    while (my $match = $find->match) {
+        push @matches, $match;
     }
 
-    return $match;
+    return @matches;
+}
+
+sub find_symbol_at_location {
+    for my $element (@_) {
+        return $element if $element->isa('PPI::Token::Symbol');
+
+        if ($element->isa('PPI::Token::Cast')) {
+            my $sibling = $element;
+            while ($sibling = $sibling->next_sibling) {
+                return $sibling if $element->isa('PPI::Token::Symbol');
+            }
+        }
+    }
+}
+
+sub find_subroutine_at_location {
+    for my $element (@_) {
+        return $element if element_is_subroutine_name($element);
+
+        if ($element->isa('PPI::Token::Cast')) {
+            my $sibling = $element;
+            while ($sibling = $sibling->next_sibling) {
+                return $sibling if element_is_subroutine_name($element);
+            }
+        }
+    }
+}
+
+sub element_is_subroutine_name {
+    my ($element) = @_;
+
+    return $element->isa('PPI::Token::Word') &&
+        Perl::Critic::Utils::is_subroutine_name($element);
 }
 
 sub find_lexical_variable_declaration {
