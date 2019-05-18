975f35c4aa2c2db89a7596bede05be508bd13054
Marc Reisner <reisner.marc@gmail.com>
2019-05-17 21:12:50 -0500

    Configure correctly so that extension works

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,7 +9,7 @@ const config = {
     target: 'node',
     entry: './src/extension.ts',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'out'),
         filename: 'extension.js',
         libraryTarget: "commonjs2",
         devtoolModuleFilenameTemplate: "../[resource-path]",
