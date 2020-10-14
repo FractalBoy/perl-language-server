19de9a2b54a51d307c0601f7fe0161a61157457e
Marc Reisner <mreisner@epic.com>
2020-10-14 11:28:19 -0500

    Add searching for method and class calls

---
+++ .vscode/perl-lang/home/mreisner/perl-language-server/server/lib/PLS/Server/Request/Workspace.pm
@@ -0,0 +1,1 @@
+{"version":3,"vars":[{"name":"PLS::Server::Request::Workspace","kind":2,"line":0,"defintion":1},{"name":"DidChangeConfiguration","containerName":"PLS::Server::Request::Workspace","kind":2,"line":2}]}
