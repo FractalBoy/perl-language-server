b716515b3f3482162b58daf20301c62c96ddfa0b
Marc Reisner <reisner.marc@gmail.com>
2020-10-13 23:45:53 -0500

    Reduce scope to only look for subroutine declarations, and do so in all files

--- server/lib/PLS/Parser/GoToDefinition.pm
+++ server/lib/PLS/Parser/GoToDefinition.pm
@@ -2,14 +2,25 @@ package PLS::Parser::GoToDefinition;
 
 use File::Spec;
 use Perl::Critic::Utils;
-use PPI;
+use PPI::Cache;
+use PPI::Document;
 use PPI::Find;
-use List::Util qw(any);
-use Scalar::Util qw(blessed);
 use URI;
+use URI::file;
+use Data::Dumper;
+
+use PLS::Server::State;
 
 sub document_from_uri {
     my ($uri) = @_;
+
+    if (length $PLS::Server::State::ROOT_PATH)
+    {
+        my $cache_path = File::Spec->catfile($PLS::Server::State::ROOT_PATH, '.pls_cache');
+        mkdir $cache unless (-d $cache_path);
+        my $ppi_cache = PPI::Cache->new(path => $cache_path);
+        PPI::Document->set_cache($ppi_cache);
+    }
 
     my $file = URI->new($uri);
     my $document = PPI::Document->new($file->file);
@@ -21,29 +32,28 @@ sub document_from_uri {
 sub lsp_location {
     my ($element) = @_;
 
-    return ($element->line_number - 1, $element->visual_column_number - 1);
+    return ($element->line_number - 1, $element->column_number - 1);
 }
 
 sub ppi_location {
     my ($line, $column) = @_;
+
+    # LSP defines position as 0-indexed, PPI defines it 1-indexed
     return (++$line, ++$column);
 }
 
 sub go_to_definition {
     my ($document, $line, $column) = @_;
 
-    # LSP defines position as 0-indexed, PPI defines it 1-indexed
     ($line, $column) = ppi_location($line, $column);
     my @matches = find_elements_at_location($document, $line, $column);
 
-    my $match = find_symbol_at_location(@matches);
-    my $definition;
-    $definition = find_lexical_variable_declaration($document, $match) if $match;
-    return lsp_location($definition) if $definition;
-    $match = find_subroutine_at_location(@matches);
-    $definition = find_subroutine_declaration($document, $match) if $match;
-    return lsp_location($definition) if $definition;
-    return ();
+    if (my $subroutine = find_subroutine_at_location(@matches))
+    {
+        return search_files_for_subroutine_declaration($subroutine->content);
+    }
+
+    return; 
 }
 
 sub find_elements_at_location {
@@ -53,8 +63,8 @@ sub find_elements_at_location {
         my ($element) = @_;
 
         $element->line_number == $line &&
-        $element->visual_column_number <= $column &&
-        $element->visual_column_number + length($element->content) >= $column;
+        $element->column_number <= $column &&
+        $element->column_number + length($element->content) >= $column;
     });
 
     return unless $find->start($document);
@@ -67,33 +77,8 @@ sub find_elements_at_location {
     return @matches;
 }
 
-sub find_symbol_at_location {
-    my $symbol;
-
-    OUTER: for my $element (@_) {
-        if ($element->isa('PPI::Token::Symbol')) {
-            $symbol = $element;
-            last;
-        }
-
-        if ($element->isa('PPI::Token::Cast')) {
-            my $sibling = $element;
-            INNER: while ($sibling = $sibling->next_sibling) {
-                if ($element->isa('PPI::Token::Symbol')) {
-                    $symbol = $element;
-                    last OUTER;
-                }
-            }
-        }
-    }
-
-    return unless $symbol;
-    return if element_is_subroutine_name($symbol); # this is a subroutine call, not a variable
-    return $symbol;
-}
-
 sub find_subroutine_at_location {
-    for my $element (@_) {
+    foreach my $element (@_) {
         return $element if element_is_subroutine_name($element);
 
         if ($element->isa('PPI::Token::Cast')) {
@@ -125,117 +110,68 @@ sub is_subroutine_name {
     return $element->sprevious_sibling eq 'sub' && $element->parent->isa('PPI::Statement');
 }
 
-sub is_forward_declaration {
-    my ($element) = @_;
+sub search_files_for_subroutine_declaration
+{
+    my ($subroutine) = @_;
 
-    return unless is_subroutine_name($element);
-    return $element->snext_sibling eq ';';
-}
-
-sub find_lexical_variable_declaration {
-    my ($document, $element) = @_;
-    return undef unless $element->isa('PPI::Token::Symbol');
-    return $element if $element->parent->isa('PPI::Statement::Variable');
-
-    # for something like for my $scalar (@array) { ... }
-    return $element if $element->parent->isa('PPI::Statement::Compound') &&
-        $element->sprevious_sibling eq 'my';
-
-    my $parent = $element;
-    while ($parent = $parent->parent) {
-        next unless $parent->scope;
-
-        # try to find variable declarations in for/foreach my $var (@array)
-        if ($parent->isa('PPI::Statement::Compound')) {
-            my $find = PPI::Find->new(sub {
-                my ($elem) = @_;
-                return $elem->isa('PPI::Token::Symbol') &&
-                    $elem->symbol eq $element->symbol &&
-                    $elem->sprevious_sibling eq 'my';
-            });
-            $find->start($parent);
-            my $match = $find->match;
-            $find->finish;
-            return $match if $match;
-        }
-
-        # try to find variable declarations in for/while/if/unless
-        my $sib = $parent->sprevious_sibling;
-        if (ref($sib) && ($sib->isa('PPI::Structure::Condition') ||
-            $sib->isa('PPI::Structure::For'))) {
-            my $find = PPI::Find->new(sub {
-                my ($elem) = @_;
-                return $elem->isa('PPI::Statement::Variable') && 
-                    grep { $_->symbol eq $element->symbol } $elem->symbols;
-            });
-
-            $find->start($sib);
-            my $match = $find->match;
-            $find->finish;
-
-            if ($match) {
-                my @symbols = $match->symbols;
-                return $symbols[0] if @symbols;
-            }
-        }
-
-        my $ok_to_look = 0;
-        for my $statement (reverse $parent->children) {
-            # since we're looking in reverse, only start looking after we find this element
-            # (i.e. this statement is before the element)
-            $ok_to_look = 1 if $statement->ancestor_of($element);
-            next unless $statement->isa('PPI::Statement::Variable') && $statement->type eq 'my';
-            next unless $ok_to_look == 1;
-            my @matches = grep { $_->symbol eq $element->symbol } $statement->symbols;
-            next unless scalar @matches;
-            return $matches[0];
-        }
-    }
-}
-
-sub sub_has_parentheses {
-    my ($element) = @_;
-
-    my $sib = $element->snext_sibling;
-    return $sib->isa('PPI::Structure::List') && $sib->complete;
-}
-
-sub sub_has_ampersand {
-    my ($element) = @_;
-
-    return $element =~ /^&/;
-}
-
-sub find_subroutine_declaration {
-    my ($document, $element) = @_;
-
-    my $subroutine_name = $element->content;
-    $subroutine_name =~ s/^&//;
+    my $perl_files = get_all_perl_files();
 
     my $find = PPI::Find->new(sub {
-        my ($elem) = @_;
-
-        return $elem->content eq $subroutine_name &&
-            is_subroutine_name($elem);
+        my ($element) = @_;
+        return 0 unless is_subroutine_name($element);
+        return $element->content eq $subroutine;
     });
 
-    $find->start($document);
-    my @matches;
+    my @results;
 
-    while (my $match = $find->match) {
-        push @matches, $match;
+    foreach my $perl_file (@$perl_files)
+    {
+        my $document = PPI::Document->new($perl_file);
+        next unless $find->start($document);
+
+        while (my $match = $find->match)
+        {
+            my ($line_number, $column_number) = lsp_location($match);
+
+            push @results, {
+                uri => URI::file->new($perl_file)->as_string,
+                range => {
+                    start => {
+                        line => $line_number,
+                        character => $column_number
+                    },
+                    end => {
+                        line => $line_number,
+                        character => ($column_number + length $match->content)
+                    }
+                }
+            }
+        }
     }
 
-    my @fwd_decl = grep { is_forward_declaration($_) } @matches;
-    @matches = grep {
-        $_->line_number < $element->line_number ||
-        scalar(@fwd_decl) && (any { $_->line_number <= $element->line_number } @fwd_decl) ||
-        is_subroutine_name($element) && $_ == $element ||
-        sub_has_parentheses($element) ||
-        sub_has_ampersand($element)
-    } @matches;
-    @matches = sort { $b->line_number <=> $a->line_number } @matches;
-    return $matches[0];
+    return \@results;
+}
+
+sub get_all_perl_files
+{
+    my @perl_files;
+
+    return unless (length $PLS::Server::State::ROOT_PATH);
+
+    File::Find::find(sub {
+        return unless -f;
+        my @pieces = File::Spec->splitdir($File::Find::name);
+        # exclude hidden files and files in hidden directories
+        return if grep { /^\./ } @pieces;
+        push @perl_files, $File::Find::name if (/\.p[ml]$/);
+        open my $code, '<', $File::Find::name or return;
+        my $first_line = <$code>;
+        push @perl_files, $File::Find::name if ($first_line =~ /^#!.*perl$/);
+        close $code;
+    }, $PLS::Server::State::ROOT_PATH);
+
+
+    return \@perl_files;
 }
 
 1;
