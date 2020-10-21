235883b5d516c58c308bea14857638172db2f77f
Marc Reisner <reisner.marc@gmail.com>
2020-10-20 22:27:50 -0500

    Fill in completion correctly

--- server/lib/PLS/Server/Response/Completion.pm
+++ server/lib/PLS/Server/Response/Completion.pm
@@ -13,19 +13,23 @@ sub new
 
     my $document = PLS::Parser::Document->new(uri => $request->{params}{textDocument}{uri});
     my @elements = $document->find_elements_at_location(@{$request->{params}{position}}{qw(line character)});
-    my ($word) = sort { length $a->name <=> length $b->name } grep { $_->{ppi_element}->significant } @elements;
+    my ($word)   = sort { length $a->name <=> length $b->name } grep { $_->{ppi_element}->significant } @elements;
 
     return $self unless (ref $word eq 'PLS::Parser::Element');
 
-    my $name = $word->name;
-    my $subs = $document->{index}{cache}{subs_trie}->find($name);
+    my $name     = $word->name;
+    my $subs     = $document->{index}{cache}{subs_trie}->find($name);
     my $packages = $document->{index}{cache}{packages_trie}->find($name);
 
-    $subs = [] unless (ref $subs eq 'ARRAY');
+    $subs     = [] unless (ref $subs eq 'ARRAY');
     $packages = [] unless (ref $packages eq 'ARRAY');
-    return $self if (not scalar @$subs    and not scalar @$packages);
+    return $self if (not scalar @$subs and not scalar @$packages);
 
     $self->{result} = [(map { {label => $_, kind => 3} } @$subs), (map { {label => $_, kind => 7} } @$packages)];
+    @{$self->{result}} = map
+    {
+        { %$_, textEdit => {newText => $_->{label}, range => $word->range} }
+    } @{$self->{result}};
 
     return $self;
 } ## end sub new
