40698a3ac739164a1a90c06e02e93a055f4eee19
Marc Reisner <reisner.marc@gmail.com>
2021-02-23 20:19:20 -0600

    Fix use parent parsing and add ISA package hover

--- server/lib/PLS/Parser/Element.pm
+++ server/lib/PLS/Parser/Element.pm
@@ -3,7 +3,9 @@ package PLS::Parser::Element;
 use strict;
 use warnings;
 
-use Scalar::Util;
+use List::Util qw(any);
+use PPIx::QuoteLike;
+use Scalar::Util qw(blessed);
 
 sub new
 {
@@ -12,7 +14,7 @@ sub new
     my %args = @args;
 
     my %self = (ppi_element => $args{element}, file => $args{file});
-    return unless (Scalar::Util::blessed($args{element}) and $args{element}->isa('PPI::Element'));
+    return unless (blessed($args{element}) and $args{element}->isa('PPI::Element'));
     return bless \%self, $class;
 } ## end sub new
 
@@ -78,88 +80,46 @@ sub package_name
     my $element = $self->{ppi_element};
     $column_number++;
 
-    if (Scalar::Util::blessed($element->parent) and $element->parent->isa('PPI::Statement::Include') and $element->parent->type eq 'use')
+    if (    blessed($element->parent)
+        and $element->parent->isa('PPI::Statement::Include')
+        and $element->parent->type eq 'use')
     {
-        # This is a 'use parent' statement. The import is a package, not a subroutine.
-        if ($element->parent->module eq 'parent' and ($element->isa('PPI::Token::Quote') or $element->isa('PPI::Token::QuoteLike')))
+        # This is a 'use parent/base' statement. The import is a package, not a subroutine.
+        if ($element->parent->module eq 'parent' or $element->parent->module eq 'base')
         {
-            if ($element->can('literal'))
-            {
-                my ($module) = $element->literal;
-                return $module, undef;
-            }
-            else
-            {
-                return $element->string, undef;
-            }
-        } ## end if ($element->parent->...)
+            my $import = _extract_import($element, $column_number);
+            return $import if (length $import);
+        }
 
         # This is likely a 'use' statement with an explicit subroutine import.
         my $package = $element->parent->module;
-
-        # Single import, single quotes or 'q' string.
-        if ($element->isa('PPI::Token::Quote::Single') or $element->isa('PPI::Token::Quote::Literal'))
-        {
-            return $package, $element->literal;
-        }
-        # Single import, double quotes or 'qq' string.
-        if ($element->isa('PPI::Token::Quote::Double') or $element->isa('PPI::Token::Quote::Interpolate'))
-        {
-            return $package, $element->string;
-        }
-        # Multiple imports, 'qw' list.
-        if ($element->isa('PPI::Token::QuoteLike::Words'))
-        {
-            my ($content)      = $element->content =~ /^qw.(.+).$/;
-            my @words          = split /(\s+)/, $content;
-            my $current_column = $element->column_number + 3;
-
-            # Figure out which word the mouse is hovering on.
-            foreach my $word (@words)
-            {
-                my $next_start = $current_column + length $word;
-
-                if ($word !~ /^\s*$/ and $current_column <= $column_number and $next_start > $column_number)
-                {
-                    return $package, $word;
-                }
-
-                $current_column = $next_start;
-            } ## end foreach my $word (@words)
-        } ## end if ($element->isa('PPI::Token::QuoteLike::Words'...))
-        # Multiple imports, using a list.
-        if ($element->isa('PPI::Structure::List'))
-        {
-            foreach my $expr ($element->children)
-            {
-                next unless $expr->isa('PPI::Statement::Expression');
-
-                foreach my $item ($expr->children)
-                {
-                    # Only handle quoted strings. Could be another or list, but that's too complicated.
-                    next unless $item->isa('PPI::Token::Quote');
-
-                    if ($item->column_number <= $column_number and ($item->column_number + length $item->content) >= $column_number)
-                    {
-                        return $package, $item->literal if ($item->can('literal'));
-                        return $package, $item->string;
-                    } ## end if ($item->column_number...)
-                } ## end foreach my $item ($expr->children...)
-            } ## end foreach my $expr ($element->...)
-        } ## end if ($element->isa('PPI::Structure::List'...))
-    } ## end if (Scalar::Util::blessed...)
+        my $import  = _extract_import($element, $column_number);
+        return $element->parent->module, $import if (length $import);
+    } ## end if (blessed($element->...))
 
     # Regular use statement, no explicit imports
     if ($element->isa('PPI::Statement::Include') and $element->type eq 'use')
     {
         return $element->module;
     }
+
+    # Class method call, cursor is over the package name
     if (    $element->isa('PPI::Token::Word')
         and ref $element->snext_sibling eq 'PPI::Token::Operator'
         and $element->snext_sibling eq '->')
     {
         return $element->content;
     } ## end if ($element->isa('PPI::Token::Word'...))
+
+    # Declaring parent class using @ISA directly.
+    if (    blessed($element->parent)
+        and $element->parent->isa('PPI::Statement::Variable')
+        and $element->parent->type eq 'our'
+        and any { $_->symbol eq '@ISA' } $element->parent->symbols)
+    {
+        my $import = _extract_import($element, $column_number);
+        return $import if (length $import);
+    } ## end if (blessed($element->...))
 
     return;
 } ## end sub package_name
@@ -244,6 +204,84 @@ sub cursor_on_package
 
     return;
 } ## end sub cursor_on_package
+
+sub _extract_import
+{
+    my ($element, $column_number) = @_;
+
+    # Single import, single quotes or 'q' string.
+    if ($element->isa('PPI::Token::Quote::Single') or $element->isa('PPI::Token::Quote::Literal'))
+    {
+        return $element->literal;
+    }
+
+    # Single import, double quotes or 'qq' string.
+    if ($element->isa('PPI::Token::Quote::Double') or $element->isa('PPI::Token::Quote::Interpolate'))
+    {
+        return $element->string;
+    }
+
+    # Multiple imports, 'qw' list.
+    if ($element->isa('PPI::Token::QuoteLike::Words'))
+    {
+        my $import = _get_string_from_qw($element, $column_number);
+        return $import if (length $import);
+    }
+
+    # Multiple imports, using a list.
+    if ($element->isa('PPI::Structure::List'))
+    {
+        my $import = _get_string_from_list($element, $column_number);
+        return $import if (length $import);
+    }
+
+    return;
+} ## end sub _extract_import
+
+sub _get_string_from_list
+{
+    my ($element, $column_number) = @_;
+
+    foreach my $expr ($element->children)
+    {
+        next unless $expr->isa('PPI::Statement::Expression');
+
+        foreach my $item ($expr->children)
+        {
+            # Only handle quoted strings. Could be another or list, but that's too complicated.
+            next unless $item->isa('PPI::Token::Quote');
+
+            if ($item->column_number <= $column_number and ($item->column_number + length $item->content) >= $column_number)
+            {
+                return $item->literal if ($item->can('literal'));
+                return $item->string;
+            }
+        } ## end foreach my $item ($expr->children...)
+    } ## end foreach my $expr ($element->...)
+} ## end sub _get_string_from_list
+
+sub _get_string_from_qw
+{
+    my ($element, $column_number) = @_;
+
+    my ($content) = $element->content =~ /qw[[:graph:]](.+)[[:graph:]]/;
+    return unless (length $content);
+    my @words = split /(\s+)/, $content;
+    my $current_column = $element->column_number + 3;
+
+    # Figure out which word the mouse is hovering on.
+    foreach my $word (@words)
+    {
+        my $next_start = $current_column + length $word;
+
+        if ($word !~ /^\s*$/ and $current_column <= $column_number and $next_start > $column_number)
+        {
+            return $word;
+        }
+
+        $current_column = $next_start;
+    } ## end foreach my $word (@words)
+} ## end sub _get_string_from_qw
 
 sub range
 {
