0febe3a0712e131f1da1de6a919f8f3fa5b350fc
Marc Reisner <mreisner@epic.com>
2021-03-24 10:47:50 -0500

    Replace $ROOT_PATH mnemonic in include dirs

--- server/lib/PLS/Server/Request/Workspace/Configuration.pm
+++ server/lib/PLS/Server/Request/Workspace/Configuration.pm
@@ -29,6 +29,16 @@ sub handle_response
     return unless (Scalar::Util::reftype $response eq 'HASH' and ref $response->{result} eq 'ARRAY');
     my $config = $response->{result}[0];
     return unless (ref $config eq 'HASH');
+    
+    # Replace $ROOT_PATH with actual workspace root in inc
+    if (exists $config->{inc} and ref $config->{inc} eq 'ARRAY')
+    {
+        foreach my $inc (@{$config->{inc}})
+        {
+            $inc =~ s/\$ROOT_PATH/$PLS::Server::State::ROOT_PATH/g;
+        }
+    }
+
     $PLS::Server::State::CONFIG = $config;
     return;
 } ## end sub handle_response
