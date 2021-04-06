c496f44d9b1518b208b042c807d1df50ab9a3a3e
Marc Reisner <mreisner@epic.com>
2021-04-06 09:12:22 -0500

    Upgrade language server client to 7.0.0

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -2,7 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
