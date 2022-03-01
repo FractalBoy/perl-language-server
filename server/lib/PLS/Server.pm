bba7ce2e41d38482130b34e352fc5f22e5cdcd6f
Marc Reisner <reisner.marc@gmail.com>
2022-02-28 18:49:38 -0600

    Cleanup

--- server/lib/PLS/Server.pm
+++ server/lib/PLS/Server.pm
@@ -16,7 +16,6 @@ use Scalar::Util qw(blessed);
 use PLS::Server::Request::Factory;
 use PLS::Server::Response;
 use PLS::Server::Response::Cancelled;
-use PLS::Server::Response::Shutdown;
 
 =head1 NAME
 
