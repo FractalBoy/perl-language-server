838a8173421d41af76397562f9ae8abac2b67a88
Marc Reisner <reisner.marc@gmail.com>
2022-08-30 16:12:49 -0500

    Throttle document symbol generation

--- server/lib/PLS/Server/Response/DocumentSymbol.pm
+++ server/lib/PLS/Server/Response/DocumentSymbol.pm
@@ -5,7 +5,10 @@ use warnings;
 
 use parent 'PLS::Server::Response';
 
+use feature 'state';
+
 use IO::Async::Loop;
+use IO::Async::Timer::Countdown;
 
 use PLS::Parser::Document;
 use PLS::Parser::DocumentSymbols;
@@ -27,24 +30,55 @@ sub new
 
     my $self = bless {id => $request->{id}, result => undef}, $class;
 
-    my $uri     = $request->{params}{textDocument}{uri};
+    my $uri = $request->{params}{textDocument}{uri};
+
+    # Delay document symbols by a couple of seconds to allow cancelling before processing starts.
+    my $future = Future->new();
+    my $timer = IO::Async::Timer::Countdown->new(
+                                                 delay            => 2,
+                                                 on_expire        => sub { $self->on_expire($uri, $future) },
+                                                 remove_on_expire => 1
+                                                );
+
+    IO::Async::Loop->new->add($timer->start());
+
+    # When the future is canceled, make sure to stop the timer so that it never actually starts generating document symbols.
+    $future->on_cancel(
+        sub {
+            $timer->stop();
+            $timer->remove_from_parent();
+        }
+    );
+
+    return $future;
+} ## end sub new
+
+sub on_expire
+{
+    my ($self, $uri, $future) = @_;
+
     my $version = PLS::Parser::Document::uri_version($uri);
 
-    return PLS::Parser::DocumentSymbols->get_all_document_symbols_async($uri)->then(
+    PLS::Parser::DocumentSymbols->get_all_document_symbols_async($uri)->on_done(
         sub {
             my ($symbols) = @_;
 
             my $current_version = PLS::Parser::Document::uri_version($uri);
-            return Future->done($self) unless (length $current_version);
-            return Future->done($self) if (length $version and $current_version > $version);
+
+            if (not length $current_version or length $version and $current_version > $version)
+            {
+                $future->done($self);
+                return;
+            }
 
             $self->{result} = $symbols;
-            return Future->done($self);
-        },
-        sub {
-            return Future->done($self);
+            $future->done($self);
         }
-    );
-} ## end sub new
+      )->on_fail(
+        sub {
+            $future->done($self);
+        }
+    )->retain();
+} ## end sub on_expire
 
 1;
