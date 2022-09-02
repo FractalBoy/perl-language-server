0ea2d8a6e5c889cd03af836089edc7c843f038f6
Marc Reisner <reisner.marc@gmail.com>
2022-09-02 16:39:19 -0500

    Warm up the symbol cache on document open

--- server/lib/PLS/Server/Request/TextDocument/DidOpen.pm
+++ server/lib/PLS/Server/Request/TextDocument/DidOpen.pm
@@ -6,6 +6,7 @@ use warnings;
 use parent 'PLS::Server::Request';
 
 use PLS::Parser::Document;
+use PLS::Parser::PackageSymbols;
 use PLS::Server::Request::TextDocument::PublishDiagnostics;
 
 =head1 NAME
@@ -27,6 +28,11 @@ sub service
     PLS::Parser::Document->open_file(%{$text_document});
 
     $server->send_server_request(PLS::Server::Request::TextDocument::PublishDiagnostics->new(uri => $text_document->{uri}));
+
+    # Warm up the cache for imported package symbols
+    my $text    = PLS::Parser::Document->text_from_uri($text_document->{uri});
+    my $imports = PLS::Parser::Document->get_imports($text);
+    PLS::Parser::PackageSymbols::get_imported_package_symbols($PLS::Server::State::CONFIG, @{$imports})->retain();
 
     return;
 } ## end sub service
