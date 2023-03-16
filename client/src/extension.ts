f70088ba4f243961275d420915b6eea0a7b2fc73
Marc Reisner <reisner.marc@gmail.com>
2023-03-16 12:36:21 -0500

    Stop adding lib path to inc

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,48 +1,46 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 import {
   LanguageClient,
   LanguageClientOptions,
   ServerOptions,
-} from 'vscode-languageclient/node';
-import { bootstrap } from './bootstrap';
+} from "vscode-languageclient/node";
+import { bootstrap } from "./bootstrap";
 
 let client: LanguageClient;
 
 export async function activate(context: vscode.ExtensionContext) {
-  const perl = vscode.workspace.getConfiguration('perl');
-  const pls = vscode.workspace.getConfiguration('pls');
-  let serverCmd = pls.get<string>('cmd') ?? perl.get<string>('pls') ?? '';
+  const perl = vscode.workspace.getConfiguration("perl");
+  const pls = vscode.workspace.getConfiguration("pls");
+  let serverCmd = pls.get<string>("cmd") ?? perl.get<string>("pls") ?? "";
   const serverArgs =
-    pls.get<string[]>('args') ?? perl.get<string[]>('plsargs') ?? [];
+    pls.get<string[]>("args") ?? perl.get<string[]>("plsargs") ?? [];
 
   const ctx = await bootstrap(context, serverCmd);
 
   if (serverCmd !== ctx.pls) {
-    pls.update('cmd', ctx.pls);
-  }
-
-  let inc = pls.get<string[]>('inc');
-
-  if (!inc) {
-    inc = [];
-  }
-
-  if (!inc || inc.indexOf(ctx.libPath.fsPath) === -1) {
-    pls.update('inc', [...inc, ctx.libPath.fsPath]);
+    pls.update("cmd", ctx.pls);
   }
 
   const serverOptions: ServerOptions = {
-    run: { command: ctx.pls, args: serverArgs, options: { env: ctx.environment } },
-    debug: { command: ctx.pls, args: serverArgs, options: { env: ctx.environment } },
+    run: {
+      command: ctx.pls,
+      args: serverArgs,
+      options: { env: ctx.environment },
+    },
+    debug: {
+      command: ctx.pls,
+      args: serverArgs,
+      options: { env: ctx.environment },
+    },
   };
 
   const clientOptions: LanguageClientOptions = {
-    documentSelector: [{ scheme: 'file', language: 'perl' }],
+    documentSelector: [{ scheme: "file", language: "perl" }],
   };
 
   client = new LanguageClient(
-    'pls',
-    'Perl Language Server (PLS)',
+    "pls",
+    "Perl Language Server (PLS)",
     serverOptions,
     clientOptions
   );
